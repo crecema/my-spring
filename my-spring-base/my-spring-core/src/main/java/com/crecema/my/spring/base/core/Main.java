70d3be05c9b521ff102d6b15a0368c05e63b12c3
crecema <crece.ma@gmail.com>
2023-04-29 00:26:14 +0800

    feat(my-spring): re-init project

---
+++ my-spring-base/my-spring-core/src/main/java/com/crecema/my/spring/base/core/Main.java
@@ -0,0 +1,9 @@
+package com.crecema.my.spring.base.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Main!");
+    }
+
+}
---
+++ my-spring-base/my-spring-core/src/test/java/com/crecema/my/spring/base/core/MainTest.java
@@ -0,0 +1,12 @@
+package com.crecema.my.spring.base.core;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello Test!");
+    }
+
+}
