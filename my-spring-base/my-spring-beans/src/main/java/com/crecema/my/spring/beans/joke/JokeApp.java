16ba3e5bfe78432f01c9fe4a3960cf075a4614ce
crecema <crece.ma@gmail.com>
2023-08-20 03:59:50 +0800

    test: add bean factory case

--- my-spring-base/my-spring-beans/src/main/java/com/crecema/my/spring/beans/joke/JokeApp.java
+++ my-spring-base/my-spring-beans/src/main/java/com/crecema/my/spring/beans/joke/JokeApp.java
@@ -1,29 +1,16 @@
 package com.crecema.my.spring.beans.joke;
 
-
 import com.crecema.my.spring.joke.launcher.JokeLauncher;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
 public class JokeApp {
 
     public static void main(String[] args) {
+
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-        BeanDefinition jokeApiClientDefinition = new GenericBeanDefinition();
-        jokeApiClientDefinition.setBeanClassName("com.crecema.my.spring.joke.common.client.JokeApiClient");
-        beanFactory.registerBeanDefinition("jokeApiClient", jokeApiClientDefinition);
-
-        BeanDefinition jokeServiceDefinition = new GenericBeanDefinition();
-        jokeServiceDefinition.setBeanClassName("com.crecema.my.spring.joke.service.impl.JokeServiceImpl");
-        jokeServiceDefinition.getConstructorArgumentValues().addGenericArgumentValue(beanFactory.getBean("jokeApiClient"));
-        beanFactory.registerBeanDefinition("jokeService", jokeServiceDefinition);
-
-        BeanDefinition jokeLauncherDefinition = new GenericBeanDefinition();
-        jokeLauncherDefinition.setBeanClassName("com.crecema.my.spring.joke.launcher.JokeLauncher");
-        jokeLauncherDefinition.getConstructorArgumentValues().addGenericArgumentValue(beanFactory.getBean("jokeService"));
-        beanFactory.registerBeanDefinition("jokeLauncher", jokeLauncherDefinition);
+        new XmlBeanDefinitionReader(beanFactory).loadBeanDefinitions("spring.xml");
 
         JokeLauncher jokeLauncher = beanFactory.getBean("jokeLauncher", JokeLauncher.class);
 
